5441e50d9af47e946922bb67bb9887c018379e9f
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-12-13 17:30:11 +0100

    git rapide avec mon make

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MyEvent } from './myEvent/myEvent.entity';
-import { MyProfile } from './profile/myProfile.entity';
 import {
   profile,
   formations,
@@ -11,7 +10,6 @@ import {
   portfolio,
 } from './myProfile/myProfile.entity';
 import { MyEventModule } from './myEvent/myEvent.module';
-import { MyProfileModule } from './profile/myProfile.module';
 import { ProfileModule } from './myProfile/myProfile.module';
 
 @Module({
@@ -23,22 +21,13 @@ import { ProfileModule } from './myProfile/myProfile.module';
       username: 'postgres',
       password: 'root',
       database: 'jarvisDev',
-      entities: [
-        MyEvent,
-        MyProfile,
-        profile,
-        formations,
-        competances,
-        portfolio,
-      ],
+      entities: [MyEvent, profile, formations, competances, portfolio],
       synchronize: true,
       autoLoadEntities: true,
       logging: false,
     }),
     MyEventModule,
-    MyProfileModule,
     ProfileModule,
-    MyProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/myEvent/myEvent.entity.ts
+++ src/myEvent/myEvent.entity.ts
@@ -1,5 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
-import { MyProfile } from './../profile/myProfile.entity';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class MyEvent {
