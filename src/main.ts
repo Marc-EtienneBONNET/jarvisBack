899b23032a012e6542a84207f99b405b43aac9dd
Marc-Etienne Bonnet <webbonnet@gmail.com>
2023-01-12 14:19:11 +0100

    syncro

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: 'http://localhost:3000',
-    origin: '*',
+    origin: [
+      'https://marc-etiennebonnet.github.io/CVV2/',
+      'http://localhost:3000',
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
